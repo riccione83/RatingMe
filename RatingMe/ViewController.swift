2285b0edc51826c80bb151eb7d95b61f0d7ae1b3
Riccardo Rizzo <r.riki@tiscali.it>
2015-06-25 16:50:26 +0200

    Initial Commit

---
+++ RatingMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RatingMe
+//
+//  Created by Riccardo Rizzo on 25/06/15.
+//  Copyright (c) 2015 Riccardo Rizzo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
